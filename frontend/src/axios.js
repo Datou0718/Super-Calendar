5361bdf03ca7d6da38e7763ed1362517b318fd60
Datou Liao <lyck92@gmail.com>
2023-01-03 13:08:30 +0800

    patch

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 const API_ROOT = "http://localhost:4000"
 const instance = process.env.NODE_ENV === "production"?
-axios.create({baseURL: "/"}) : axios.create({ baseURL: API_ROOT })
+axios.create({baseURL: "/login"}) : axios.create({ baseURL: API_ROOT })
 
 export default instance;
